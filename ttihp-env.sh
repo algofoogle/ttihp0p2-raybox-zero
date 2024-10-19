d8d047a834de5b6dc72eeaadf9b8dd6ae697779c
Anton Maurovic <anton@foogle.com>
2024-10-19 20:57:20 +1030

    Tweak for 4x2 harden

---
+++ ttihp-env.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export FLOW=orfs
+export FLOW_ARG=--orfs
+export IHP_PDK_ROOT=/home/anton/ttsetup@tt09-ihp/pdk
+export IHP_PDK_COMMIT=69c58d9ba56d8894c1499b2dd2689f5e22692704
+export ORFS_ROOT=/home/anton/ttsetup@tt09-ihp/orfs
+export ORFS_COMMIT=ad1c669e5bfaf57b2b6e9c017c7255182d461190
+export OPENROAD_EXE=/usr/bin/openroad
+export YOSYS_EXE=/home/anton/ttsetup@tt09-ihp/oss-cad-suite/bin/yosys
+source /home/anton/ttsetup@tt09-ihp/venv/bin/activate
+
