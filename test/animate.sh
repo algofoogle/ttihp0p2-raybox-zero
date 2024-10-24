895669f1d1613e2bd739efd0bf320ed65740a4b7
Anton Maurovic <anton@foogle.com>
2024-10-25 07:22:51 +1030

    Try debugging on MP4 preview, and add animated GIF preview

--- test/animate.sh
+++ test/animate.sh
@@ -4,9 +4,10 @@ if [ $# -ne 1 ]; then
 cat <<EOH
 Usage: $0 OUTSPEC
 Where:
-  OUTSPEC is one of:
+  OUTSPEC is ONE of:
   - a dir for writing a sequence of PNG files
   - a filename ending in .mp4
+  - a filename ending in .gif (animated gif)
   - a double dash (--) for Anton's default behaviour
 EOH
 exit 1
@@ -17,6 +18,11 @@ function write_h264() {
     ffmpeg -framerate 60 -i frames_out/rbz_basic_frame-%03d.ppm -c:v libx264 -r 60 "$1"
 }
 
+function write_animated_gif() {
+    #NOTE: -delay 50 sets a 50*10ms (500ms) delay between frames:
+    convert -delay 50 rbz_basic_frame-???.ppm +antialias -font Ubuntu-Mono -fill white -pointsize 18 -gravity South -annotate 0 '%f' "$1"
+}
+
 function anton_default1() {
     outfile="$HOME/HOST_Documents/rbz-$(date +%Y%m%d_%H%M%S).mp4"
     write_h264 "$outfile" \
@@ -24,28 +30,23 @@ function anton_default1() {
         || echo "Failed to write: $outfile"
 }
 
-## Lossless FFV1 (mkv usually) file:
-#function write_ffv1() {
-#    ffmpeg -framerate 60 -i frames_out/rbz_basic_frame-%03d.ppm -c:v ffv1 -r 60 "$1"
-#}
-#
-#
-#function anton_default2() {
-#    outfile="$HOME/HOST_Documents/rbz-$(date +%Y%m%d_%H%M%S).mkv"
-#    write_ffv1 "$outfile" \
-#        && echo "Wrote output file: $outfile" \
-#        || echo "Failed to write: $outfile"
-#}
-
 case "$1" in
+    # -- was given: Just do a default per Anton's own local testing:
     --)
         anton_default1
         ;;
 
+    # Target .mp4 filename was given:
     *.mp4)
         write_h264 "$1"
         ;;
 
+    # Target .gif filename was given:
+    *.gif)
+        write_animated_gif "$1"
+        ;;
+
+    # Target subdirectory name for discrete PNG files was given:
     *)
         # Convert PPM sequence to PNG sequence in $1 dir:
         # pushd frames_out
