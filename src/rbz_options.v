03ca659bc708415fb64011dbda29087af6411a97
Anton Maurovic <anton@foogle.com>
2024-10-27 19:07:29 +1030

    Move raybox-zero optional defines to separate file

---
+++ src/rbz_options.v
@@ -0,0 +1,7 @@
+`define USE_MAP_OVERLAY
+`define USE_DEBUG_OVERLAY
+//`define TRACE_STATE_DEBUG  // Trace state is represented visually per each line on-screen.
+//`define STANDBY_RESET // If defined use extra logic to avoid clocking regs during reset (for power saving/stability).
+//`define RESET_TEXTURE_MEMORY // Should there be an explicit reset for local texture memory?
+//`define RESET_TEXTURE_MEMORY_PATTERNED // If defined with RESET_TEXTURE_MEMORY, texture memory reset is a pattern instead of black.
+//`define DEBUG_NO_TEXTURE_LOAD // If defined, prevent texture loading
